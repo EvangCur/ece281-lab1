b39c558d582bafb4998ed6299960116cbf6486a0
EvangCur <c25curt.evangelister@afacademy.af.edu>
2024-02-08 10:33:14 -0700

    Final working version

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -71,11 +71,20 @@ architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
-	w_sel(0) <= i_C;	-- one
-	w_sel(1) <= i_B;
-	w_sel(2) <= i_D;
+	w_sel(0) <= i_D;	-- one
+	w_sel(1) <= i_C;
+	w_sel(2) <= i_B;
 	--finish assigning signals
 	
-	o_Y <= (not i_A and i_D) or (i_A and not i_D);--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+	o_Y <= i_A when (w_sel = "000") else
+           not i_A when (w_sel = "001") else
+           i_A when (w_sel = "010") else
+           not i_A when (w_sel = "011") else
+           i_A when (w_sel = "100") else
+           not i_A when (w_sel = "101") else
+           i_A when (w_sel = "110") else
+           not i_A when (w_sel = "111") else
+           '0';
+--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
